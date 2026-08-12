e15fb22bfd40bf60d5e97005dae6b7e3edb027c9
Fatema <fatemazakaria666@gmail.com>
2026-08-12 21:52:57 +0300

    Add package.json

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.v4.css"; // <-- this import is what actually applies Tailwind
+import "./index.css"; // <-- this import is what actually applies Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
